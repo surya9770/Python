7a0afc11a6d7a5bd9863c0a6761fa73e5d6b18a1
Surya Kant Tiwari <129664066+surya9770@users.noreply.github.com>
2025-08-25 22:50:59 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,17 @@
+print("Enter the first number :")
+firstnumber=int(input())
+print("Enter the second number :")
+secondnumber=int(input())
+
+Addition=firstnumber+secondnumber
+print("Addition:")
+print(Addition)
+Subtraction=firstnumber-secondnumber
+print("Subtraction:")
+print(Subtraction)
+Multiplication=firstnumber*secondnumber
+print("Multiplication:")
+print(Multiplication)
+Division=firstnumber/secondnumber
+print("Division:")
+print(Division)
---
+++ greetings.py
@@ -0,0 +1,5 @@
+from tokenize import String
+
+a=input(print("Enter Your first name:"))
+b=input(print("Enter Your second name:"))
+print("Hello, "+a +" "+b+"! Welcome to the Python program.")
